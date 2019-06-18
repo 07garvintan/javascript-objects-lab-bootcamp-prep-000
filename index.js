61fcd1d1984f63e00e20b2d26899184e884612b0
garvin tan <garvintan07@gmail.com>
2019-06-18 23:05:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
 function updateObjectWithKeyAndValue(object,key,value) {
   return Object.assign({}, object, {[key]:value})
 }
+
+function destructivelyDeleteFromObjectByKey(object,key) {
+  delete object[key]
+  return object
+}
